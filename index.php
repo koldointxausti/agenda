c3a49375195751498df2bbaaed739d2f8872343f
koldointxausti <koldointxaustii@gmail.com>
2019-10-08 09:37:12 +0200

    mantener datos en los input al hacer submit

--- index.php
+++ index.php
@@ -6,47 +6,32 @@ session_start();
 <head>
 	<title>Agenda</title>
 	<meta charset="utf-8">
-	<style>
-		#agenda{
-			margin-top: 5%;
-			display: grid;
-			grid-template-columns: 25% 25% 25% 25%;
-		}
-		.contacto{
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-			align-items: center;
-			box-shadow: 1px 1px 7px lightgray;
-			max-width: 70%;
-			padding: 10% 5%;
-			margin: 1% 0;
-		}
-		.name{
-			font-weight: bold;
-			padding-bottom: 5%;
-			text-transform: uppercase;
-			letter-spacing: 5px;
-		}
-	</style>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
 	<form action="" method="POST">
-		<input type="text" name="nombre" placeholder="Nombre" pattern="[A-Za-z]+" required>
-		<input type="email" name="email" placeholder="Correo electrónico">
+		<input type="text" name="nombre" placeholder="Nombre" pattern="[A-Za-z]+" required value=<?php
+			if(!empty($_POST['submit']))
+				echo $_POST['nombre'];
+		?>>
+		<input type="email" name="email" placeholder="Correo electrónico" value=<?php
+			if(!empty($_POST['submit']) && !empty($_POST['email']))
+				echo $_POST['email'];
+		?>>
 		<input type="submit" name="submit" value="Añadir">
 	</form>
 	<div id="agenda">
 		<?php
-			if(!empty($_POST['nombre']) && !empty($_POST['email'])){
-				$_SESSION['agenda'][$_POST['nombre']] = $_POST['email'];
-			}elseif(empty($_POST['email']) && !empty($_POST['nombre'])){
-				unset($_SESSION['agenda'][$_POST['nombre']]);
-			}else{
-				echo 'Introduce un contacto';
-			}
-			foreach($_SESSION['agenda'] as $nombre => $email)
-				echo '<div class="contacto"><span class="name">'.$nombre.'</span><span>'.$email.'</span></div>';
+			if(!empty($_POST['submit'])){
+				if(!empty($_POST['nombre']) && !empty($_POST['email'])){
+					$_SESSION['agenda'][$_POST['nombre']] = $_POST['email'];
+				}elseif(empty($_POST['email']) && !empty($_POST['nombre'])){
+					unset($_SESSION['agenda'][$_POST['nombre']]);
+				}
+				foreach($_SESSION['agenda'] as $nombre => $email)
+					echo '<div class="contacto"><span class="name">'.$nombre.'</span><span>'.$email.'</span></div>';	
+			}else
+				echo 'Introduce los datos solicitados';
 		?>
 	</div>
 </body>
